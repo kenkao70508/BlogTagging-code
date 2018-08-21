b15095fd888f4fbe07a1db1ce02bbf117b80cf33
Kuang Kao <kenkao70508@gmail.com>
2018-08-21 22:57:26 +0800

    update

--- gitpush.py
+++ gitpush.py
@@ -2,8 +2,6 @@ import os, pexpect, time
 
 os.system('git add .')
 os.system('git commit -m "update"')
-os.system('git push')
-time.sleep(5)
-os.system('kenkao70508')
-time.sleep(3)
-os.system('nw344556')
+child = pexpect.spawn('git push')
+child.expect('Username for "https://github.com"')
+child.sendline(kenkao70508)
