82898903ab8e23c49442367312e28cbadee48845
Kuang Kao <kenkao70508@gmail.com>
2018-08-21 18:22:11 +0800

    update

--- check-doneData.py
+++ check-doneData.py
@@ -2,5 +2,13 @@ import os, json
 
 path_to_doneData = '../doneData/'
 jsonFilesIndoneData = [ jsonFile for jsonFile in os.listdir(path_to_doneData) ]
+totalNumber = 0
 
-print(jsonFilesIndoneData)
+for file in jsonFilesIndoneData:
+    with open(path_to_doneData + file, 'r') as f: 
+        read_json = json.load(f)
+        totalNumber += len(read_json)
+        print("FileName:{} ; NumberofArticles:{}".format(path_to_doneData + file, len(read_json)))
+
+print("Total:", totalNumber)
+
