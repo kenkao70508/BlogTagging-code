1c733ae425c6bc39d7d865475a6305af10227970
Kuang Kao <kenkao70508@gmail.com>
2018-08-21 21:18:49 +0800

    update guest

--- dataUpdateKeys.py
+++ dataUpdateKeys.py
@@ -7,7 +7,6 @@ def updateJsonFile(filename, filepath):
     # update articles keys
     with open(filepath + filename, 'r') as json_file:
         readJson = json.load(json_file)
-        print(filepath + filename, readJson[0].keys())
         for item in readJson:
             item['assigned']  = "no"
             item['annotater'] = ""
@@ -27,6 +26,14 @@ if __name__ == '__main__':
     for folderName in userData_folders: 
         path_to_userData_user = path_to_userData + folderName + '/'  
         user_categories = [name for name in os.listdir(path_to_userData_user) if os.path.isdir(os.path.join(path_to_userData_user,name))]
-        print("user:{}; categories:{}".format(folderName, user_categories))
+        print("="*20)
+        print("CURRENT: user:{}; categories:{}".format(folderName, user_categories))
+        for cat in user_categories:
+            path_to_userData_user_cat = path_to_userData_user + cat + '/'
+            json_files = [name for name in os.listdir(path_to_userData_user_cat) if name.endswith('.json')]
+            print("CATEGORY:{}".format(cat))
+            for filename in json_files:
+                updateJsonFile(filename, path_to_userData_user_cat)
+                print("File:{}".format(filename))
     
 
