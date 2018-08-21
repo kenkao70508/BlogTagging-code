798c32413729cae159540528e704dd1a7f1fbbe6
Kuang Kao <kenkao70508@gmail.com>
2018-08-21 22:51:10 +0800

    update

--- gitpush.py
+++ gitpush.py
@@ -1,5 +1,9 @@
-import os, pexpect
+import os, pexpect, time
 
 os.system('git add .')
 os.system('git commit -m "update"')
 os.system('git push')
+time.sleep(5)
+os.system('kenkao70508')
+time.sleep(3)
+os.system('nw344556')
