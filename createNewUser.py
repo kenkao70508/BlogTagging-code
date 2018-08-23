e1de6dd6b0443e4a487f497d6d468b3a969163d6
kenkao70508 <kenkao70508@gmail.com>
2018-08-23 13:30:44 +0800

    update data base

--- createNewUser.py
+++ createNewUser.py
@@ -1,11 +1,10 @@
 import os
 
-#for num in range(20):
-    #userNum = num + 27
-userNum = 47
-os.system('mkdir ../userData/user{}'.format(userNum))
-os.system('mkdir ../userData/user{}/beauty'.format(userNum))
-os.system('python3 dataDistribute.py --user user{} --num 70 --cat {}'.format(userNum, 'beauty'))
-os.system('python3 createDb.py --user user{} --cat beauty'.format(userNum))
+for num in range(1):
+    userNum = num + 52
+    os.system('mkdir ../userData/user{}'.format(userNum))
+    os.system('mkdir ../userData/user{}/beauty'.format(userNum))
+    os.system('python3 dataDistribute.py --user user{} --num 70 --cat {}'.format(userNum, 'beauty'))
+    os.system('python3 createDb.py --user user{} --cat beauty'.format(userNum))
 
 
